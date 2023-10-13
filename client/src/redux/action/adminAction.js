1e7de7124dd950a043b3c212fa127d1622303ea6
vasanth8017 <91736638+vasanth8017@users.noreply.github.com>
2023-10-13 14:12:39 +0530

    Update adminAction.js

--- client/src/redux/action/adminAction.js
+++ client/src/redux/action/adminAction.js
@@ -6,7 +6,7 @@ import {
     GET_SUBJECTS
 } from '../actionTypes'
 
-const url = "http://localhost:5000"
+const url = "https://attns-server.onrender.com/"
 
 const setAdmin = (data) => {
     return {
